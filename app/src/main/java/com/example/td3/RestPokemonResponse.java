a454f11446fa2ec67be3467fd255d4ab28faa733
Denis <ahratina@et.esiea.fr>
2020-05-10 13:40:55 +0200

    TD4

---
+++ app/src/main/java/com/example/td3/RestPokemonResponse.java
@@ -0,0 +1,33 @@
+package com.example.td3;
+
+import java.util.List;
+
+public class RestPokemonResponse {
+    private Integer count;
+    private String next;
+    private List<Pokemon> results;
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public List<Pokemon> getResults() {
+        return results;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public void setResults(List<Pokemon> results) {
+        this.results = results;
+    }
+}
